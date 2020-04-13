de5231b254b6325aeeba4eaeb020a929123c1a48
Hakan Yildiz <hakan@re-coded.com>
2020-04-13 17:46:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+class Polygon{
+  constructor(arr){
+    this.arr = arr;
+  }
+  
+  get countSides(){
+    return this.arr.length;
+  }
+  
+  get perimeter(arr){
+    let total = (acc,cur) => acc +curr;
+    let sum = this.arr.reduce(total)
+      return sum;
+   
+  }
+}
